f9adaf6124925f6722463b577afc63bdde7e77d0
Jonathan <jonathankyule2@gmail.com>
2024-01-09 05:01:27 -0800

    adding stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,24 @@ Jsonifying code
 """
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route('api/v1/stats', methods=['GET'], strict_slashes=False)
+def stats():
+    classes = {
+            'amenities': 'Amenity',
+            'cities': 'City',
+            'places': 'Place',
+            'reviews': 'Review',
+            'states': 'States',
+            'users': 'User'
+            }
+    stats_dict = {}
+    for key, value in classes.items():
+        stats_dict[key] = storage.count(value)
+
+    return jsonify(stats_dict)
