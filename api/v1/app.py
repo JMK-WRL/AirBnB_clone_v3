89148743c9bdcab14fda30f7a2651dd0d88678f8
Jonathan <jonathankyule2@gmail.com>
2024-01-09 11:24:27 -0800

    updates

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@ Commands to start the server
 
 import os
 from flask import Flask
+from werkzeug.exceptions import HTTPException
 from api.v1.views import app_views
 from models import storage
 
@@ -17,6 +18,10 @@ def teardown(exception):
     Closes the storage on teardown
     """
     storage.close()
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
